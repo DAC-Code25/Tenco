309f31baeba8bd21b0d18ce201ab5ee42d3a172f
MrHaiJun <1497241294@qq.com>
2026-05-22 14:11:29 +0800

    完善帮助界面现场手册 (#9)
    补齐帮助界面内容结构、现场手册、快捷键说明和对应文档。

--- help.h
+++ help.h
@@ -2,6 +2,15 @@
 #define HELP_H
 
 #include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QVector>
+
+class QLabel;
+class QListWidget;
+class QListWidgetItem;
+class QStackedWidget;
+class QWidget;
 
 namespace Ui {
 class MainWindow;
@@ -15,7 +24,41 @@ public:
     explicit Help(Ui::MainWindow *ui, QObject *parent = nullptr);
 
 private:
+    struct DocumentLink {
+        QString title;
+        QString description;
+        QString relativePath;
+    };
+
+    void buildUi();
+    QWidget *createThemePage(const QString &title, const QString &description);
+    QWidget *createQuickStartPage();
+    QWidget *createOverviewPage();
+    QWidget *createSafetyPage();
+    QWidget *createSopPage();
+    QWidget *createHomePage();
+    QWidget *createMapWorkPage();
+    QWidget *createMaintenancePage();
+    QWidget *createConnectionPage();
+    QWidget *createOperationsPage();
+    QWidget *createTroubleshootingPage();
+    QWidget *createShortcutPage();
+    QWidget *createDocumentPage();
+    QWidget *createDataLogPage();
+    QWidget *createRuntimePage();
+
+    QWidget *createCard(const QString &title, const QStringList &lines, QWidget *parent = nullptr) const;
+    QWidget *createStepCard(const QString &title, const QStringList &steps, QWidget *parent = nullptr) const;
+    QWidget *createInfoRow(const QString &label, const QString &value, QWidget *parent = nullptr) const;
+    void addTopic(const QString &title, QWidget *page);
+    void openPath(const QString &path, const QString &errorTitle) const;
+    QString appDataPath(const QString &child) const;
+    QString repositoryPath(const QString &relativePath = QString()) const;
+
     Ui::MainWindow *ui;
+    QListWidget *m_navList = nullptr;
+    QStackedWidget *m_stack = nullptr;
+    QVector<DocumentLink> m_documents;
 };
 
 #endif // HELP_H
